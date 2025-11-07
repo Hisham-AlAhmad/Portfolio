ff59e8e18f21a1099eb29d142f8b489c2b188423
Hisham Al Ahmad <hishamalahmadx@gmail.com>
2025-11-07 16:12:35 +0200

    Updated BackToTop

--- src/components/BackToTop/BackToTop.jsx
+++ src/components/BackToTop/BackToTop.jsx
@@ -32,18 +32,7 @@ const BackToTop = () => {
             className={`back-to-top ${isVisible ? 'show' : ''}`}
             aria-label="Back to top"
         >
-            <svg
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            >
-                <path d="M12 19V5M5 12l7-7 7 7"/>
-            </svg>
+            <i className="ti ti-arrow-up"></i>
         </button>
     );
 };
